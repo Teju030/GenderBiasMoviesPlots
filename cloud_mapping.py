1b8cc6e70e7530635e41ee846fafb8188e7cefd8
vaishnaviK06 <kotawarvaishnavi@gmail.com>
2021-11-20 19:46:31 -0800

    Addition of Gender

--- cloud_mapping.py
+++ cloud_mapping.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pandas.core import indexing
+from gender_predictor.GenderClassifier import classify_gender
 
 
 def read_input_file(filename):
@@ -28,13 +29,15 @@ def get_name_and_adjective_mapping(all_movie_plots):
         #adjective = movie[int(name_data['governor'])]
             
             character_name = name['dependent']
+            gender = classify_gender(character_name)
+
             if character_name not in character_adj.keys():
-                character_adj[character_name] = []
+                character_adj[character_name] = {"adjectives":[],"gender":gender}
 
             governor = int(name['governor'])
             governor_df = movie[movie['token_id'] == governor]
             if governor_df['dep_pos'].values[0] == 'JJ':
-                character_adj[character_name].append(governor_df['dependent'].values[0])
+                character_adj[character_name]["adjectives"].append(governor_df['dependent'].values[0])
     
         name_adj_cluster_all_list.append(character_adj) 
 
@@ -46,8 +49,7 @@ def get_adjective_cloud(filename):
     all_movie_plots = get_plots_by_movie_id(movie_data_df)
     name_adj_cluster_list = get_name_and_adjective_mapping(all_movie_plots)
     print(name_adj_cluster_list)
-    
-    
+
 
 #get_adjective_cloud("SampleDependency.txt")
 
